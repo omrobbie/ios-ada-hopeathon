d22c1b2a1a2ff8e0d21cd81d4c9e1be9b7f580ad
Novie David <noviedavid@yahoo.com>
2018-09-04 22:36:23 +0700

    Project Details

--- Hopeathon/Project/ProjectViewController.swift
+++ Hopeathon/Project/ProjectViewController.swift
@@ -9,13 +9,26 @@
 import UIKit
 import JJFloatingActionButton
 
-class ProjectViewController: UIViewController {
+class ProjectViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tblviewProjects: UITableView!
     
     let actionButton = JJFloatingActionButton()
+    
+    var arrProjects = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tblviewProjects.delegate = self
+        tblviewProjects.dataSource = self
+        
+        arrProjects.append("Gempa Lombok")
+        arrProjects.append("Banjir Bali")
+        arrProjects.append("Tsunami Aceh")
+        
+        
+        
         actionButton.buttonColor = #colorLiteral(red: 0.1490196078, green: 0.6, blue: 0.9843137255, alpha: 1)
         
         actionButton.addItem(title: "Add") { item in
@@ -25,7 +38,30 @@ class ProjectViewController: UIViewController {
         actionButton.display(inViewController: self)
     }
 
-    @IBAction func btnDetailProjectClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toDetailProject", sender: sender)
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrProjects.count
     }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellProjects", for: indexPath) as! ProjectsTableViewCell
+        
+        let pName = arrProjects[indexPath.row]
+        print(pName)
+        //cell.cellProtocol = self
+        
+        cell.lblProjectName.text = pName
+        
+        
+        return cell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "toDetailProject", sender: indexPath)
+    }
+    
+    
 }
---
+++ Hopeathon/Project/ProjectsTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  ProjectsTableViewCell.swift
+//  Hopeathon
+//
+//  Created by Novie David on 04/09/18.
+//  Copyright © 2018 omrobbie. All rights reserved.
+//
+
+import UIKit
+
+class ProjectsTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var lblProjectName: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
